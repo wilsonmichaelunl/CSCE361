3432625dd500442fd8b3efcc3c7f4b99ae883239
wilsonmichaelunl <wilson.michaelthomas@gmail.com>
2018-03-07 21:32:58 -0600

    added basement floor to avery

--- app/src/main/java/com/carlscrate/unl_bathroom/AveryActivity.java
+++ app/src/main/java/com/carlscrate/unl_bathroom/AveryActivity.java
@@ -1,7 +1,10 @@
 package com.carlscrate.unl_bathroom;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class AveryActivity extends Activity {
@@ -10,5 +13,32 @@ public class AveryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_avery);
+
+        //Called when a floor is selected from the list view
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> listView,
+                                    View floorView,
+                                    int position,
+                                    long id) {
+                switch (position) {
+                    case 0:
+                        //The intent is coming from AveryActivity, it needs to launch FloorActivity
+                        Intent intent = new Intent(AveryActivity.this, AveryBasementFloor.class);
+                        startActivity((intent));
+                        break;
+
+                    default:
+                        intent = new Intent(AveryActivity.this, UnderConstruction.class);
+                        startActivity(intent);
+                        break;
+                }
+
+
+            }
+        };
+
+        //Adding the listener to the list view
+        ListView listView = (ListView) findViewById(R.id.avery_floors);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
