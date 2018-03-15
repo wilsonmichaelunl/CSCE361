17d7f84e974b251dee410676b3d348059a76a154
Fei Yu <feihelen1996@gmail.com>
2018-03-14 23:25:22 -0500

    added avery women bathroom

---
+++ app/src/main/java/com/carlscrate/unl_bathroom/AveryWomens1Activity.java
@@ -0,0 +1,14 @@
+package com.carlscrate.unl_bathroom;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AveryWomens1Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avery_womens1);
+    }
+}
